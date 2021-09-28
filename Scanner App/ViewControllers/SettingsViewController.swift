fed65895c48856d21ad96552f5fef24f61f81b86
Scott <91510798+Scott-776@users.noreply.github.com>
2021-09-28 17:16:44 -0600

    Update SettingsViewController.swift

--- Scanner App/ViewControllers/SettingsViewController.swift
+++ Scanner App/ViewControllers/SettingsViewController.swift
@@ -3,12 +3,10 @@
 //  Scanner App
 //
 //
-
 import UIKit
 import MessageUI
 import SafariServices
 class SettingsViewController: UITableViewController, MFMailComposeViewControllerDelegate  {
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let titles = [NSLocalizedString("terms", comment: "terms"),NSLocalizedString("pp", comment: "privacy"),NSLocalizedString("contact", comment: "contact")]
         let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
@@ -16,12 +14,10 @@ class SettingsViewController: UITableViewController, MFMailComposeViewController
         cell.accessoryType = .disclosureIndicator
         return cell
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
-    
-  
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,59 +27,52 @@ class SettingsViewController: UITableViewController, MFMailComposeViewController
         navigationController?.navigationBar.isTranslucent = false
         if #available(iOS 13.0, *) {
             navigationController?.navigationBar.barTintColor = UIColor.systemBackground
-        } else {
+        }
+        else {
             navigationController?.navigationBar.barTintColor = UIColor.white
         }
         // Do any additional setup after loading the view.
     }
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-         if indexPath.row == 0{
-            if let url = URL(string: ""){
-                
-                let config = SFSafariViewController.Configuration()
-                config.entersReaderIfAvailable = true
-                let vc = SFSafariViewController(url: url, configuration: config)
-               
-                
-                present(vc, animated: true, completion: nil)
-            }
-        }
-         if indexPath.row == 1{
+        if indexPath.row == 0{
             if let url = URL(string: ""){
                 let config = SFSafariViewController.Configuration()
                 config.entersReaderIfAvailable = true
                 let vc = SFSafariViewController(url: url, configuration: config)
-                
-                
-                
-                
                 present(vc, animated: true, completion: nil)
             }
         }
-         if indexPath.row == 2{
+        if indexPath.row == 1{
+            if let url = URL(string: ""){
+                let config = SFSafariViewController.Configuration()
+                config.entersReaderIfAvailable = true
+                let vc = SFSafariViewController(url: url, configuration: config)
+                present(vc, animated: true, completion: nil)
+            }
+        }
+        if indexPath.row == 2{
             if MFMailComposeViewController.canSendMail(){
                 let mail = MFMailComposeViewController()
                 mail.mailComposeDelegate = self
-               
                 mail.setToRecipients([""])
                 mail.setSubject("Support")
                 present(mail, animated: true, completion: nil)
-            }else{
-                
+            }
+            else{
                 if let url = URL(string: ""){
                     UIApplication.shared.open(url, options: [ : ], completionHandler: nil)
                 }
             }
         }
     }
+
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         controller.dismiss(animated: true, completion: nil)
-        
-        
     }
 
     @IBAction func close(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
+
 }
