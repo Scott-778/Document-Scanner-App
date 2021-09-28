cd93a7f8885fd22534d849e0ec3e8e0ba6a852e3
Scott <91510798+Scott-776@users.noreply.github.com>
2021-09-28 17:18:20 -0600

    Update DocumentTableViewCell.swift

--- Scanner App/Views/DocumentTableViewCell.swift
+++ Scanner App/Views/DocumentTableViewCell.swift
@@ -3,16 +3,10 @@
 //  Scanner App
 //
 //
-
 import UIKit
-
 class DocumentTableViewCell: UITableViewCell {
-   
     @IBOutlet weak var docImage: UIImageView!
-    
     @IBOutlet weak var titleLabel: UILabel!
-    
     @IBOutlet weak var dateLabel: UILabel!
-    
     @IBOutlet weak var pageCountLabel: UILabel!
 }
